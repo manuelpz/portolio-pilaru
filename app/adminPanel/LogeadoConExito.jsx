5dbbc578c5d346bcfd7fc23e905ee6b6325bb7c8
Manuel Lopez Fernandez <manuel.lopez.fernandez97@gmail.com>
2024-01-03 17:46:09 +0100

    Se empieza a insertar las opciones de borrado

--- app/adminPanel/LogeadoConExito.jsx
+++ app/adminPanel/LogeadoConExito.jsx
@@ -7,13 +7,13 @@ export default function LogeadoConExito() {
             <h1 className="text-center font-bold uppercase text-2xl">¡Hola! 👋🏽 ¿Qúe vamos a hacer?</h1>
             <br />
             <br />
-            <div className="lg:flex lg:justify-center lg:space-x-20">
+            <div className="lg:grid lg:grid-cols-5 lg:justify-center ">
                 <CardOption titulo='Subir una noticia' componente='/subidas/noticias' imagen={'/adminPanel/noticias.png'} />
                 <CardOption titulo='Subir un video' componente='/subidas/videos' imagen={'/adminPanel/videos.png'} />
                 <CardOption titulo='Subir una entrevista' componente='/subidas/entrevistas' imagen={'/adminPanel/entrevistas.png'} />
                 <CardOption titulo='Subir un podcast' componente='/subidas/podcasts' imagen={'/adminPanel/podcasts.png'} />
-                <CardOption titulo='Subir un reconociemto' componente='/subidas/reconocimientos' imagen={'/adminPanel/reconocimientos.png'} />
-
+                <CardOption titulo='Subir un reconociento' componente='/subidas/reconocimientos' imagen={'/adminPanel/reconocimientos.png'} />
+                <CardOption titulo='Eliminar una entrevista' componente='/borrados/entrevistas' imagen={'/adminPanel/entrevistas_borrado.png'} />
             </div>
         </>
     )
