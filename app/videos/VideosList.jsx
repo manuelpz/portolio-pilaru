5a4ced9c38affb50c95bb5365aab6991feac63ac
Manuel Lopez Fernandez <manuel.lopez.fernandez97@gmail.com>
2024-01-11 00:08:07 +0100

    texto justificado y margenes

--- app/videos/VideosList.jsx
+++ app/videos/VideosList.jsx
@@ -20,11 +20,13 @@ export default async function NoticiasList() {
         </div>
     )
     return (
-        <div className="grid h-1/2 lg:grid-cols-2 lg:justify-items-center">
+        <div className="grid  h-1/2 lg:grid-cols-2 lg:justify-items-center lg:ml-10">
             {videos.slice(-12).map((video) => (
                 <div key={video.id} className={`aparicion mt-12 scroll-animation w-full`}>
                     <h1 className="text-center font-bold uppercase text-xl">{video.titulo}</h1>
-                    {video.comentario && <p className="text-center">{video.comentario}</p>}
+                    <br />
+                    {video.comentario && <p className="text-justify mx-6 lg:mx-12">{video.comentario}</p>}
+                    <br />
                     <div className="flex mt-4 justify-center lg:m-4">
                         <video width={640} height={480} controls poster={video.poster} className='lg:rounded'>
                             <source src={video.video} type="video/mp4" />
