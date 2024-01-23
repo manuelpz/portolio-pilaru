a14d77d5d3aca18c2ced59b907412df663ff1c4b
Manuel Lopez Fernandez <manuel.lopez.fernandez97@gmail.com>
2024-01-23 23:53:58 +0100

    Remove unused code and update podcast display

--- app/entrevistas/EntrevistasList.jsx
+++ app/entrevistas/EntrevistasList.jsx
@@ -1,5 +1,4 @@
 import '@/app/globals.css'
-import Image from 'next/image'
 import CarpetaVacia from '@/components/CarpetaVacia/CarpetaVacia'
 
 const URL_BASE_ENTREVISTAS = 'https://portfolio-pilaru-back.onrender.com/api/entrevistas'
@@ -25,22 +24,11 @@ export default async function EntrevistasList() {
             {entrevistas.slice(0, 12).map((entrevista) => (
                 <div key={entrevista.id}
                     className="grid grid-cols-1 justify-items-center aparicion scroll-animation mb-12
-                               lg:mb-32
-                               xl:grid-cols-2 xl:pr-15" >
-                    <Image className="rounded shadow-2xl justify-self-center w-28 h-28
-                                      xl:w-48 xl:h-48
-                                      2xl:justify-self-end"
-                        src={entrevista.img ?? '/logo/icono-pilar-ramos.png'}
-                        alt="Imagen relacionada con el podcast"
-                        width={200}
-                        height={10} />
-                    <div className="m-4
-                                    lg:grid lg:content-end lg:justify-items-start">
+                               lg:mb-32" >
+                    <div>
                         <h1 className="text-center font-bold uppercase mb-5 mt-5
                                        lg:mb-10">{entrevista.titulo}</h1>
-                        <audio className="bg-blue-200 2xl:w-80 p-2 rounded-lg shadow-md justify-self-end" controls>
-                            <source src={entrevista.entrevista} type="audio/mp3" />
-                        </audio>
+                        <div dangerouslySetInnerHTML={{ __html: entrevista.entrevista }} />
                     </div>
                 </div>
             ))}
--- app/subidas/entrevistas/subida.jsx
+++ app/subidas/entrevistas/subida.jsx
@@ -3,74 +3,61 @@ import AdminValidation from '@/components/AdminValidation/AdminValidation'
 import BotonVolver from '@/components/BotonVolver/BotonVolver'
 import { useState } from 'react'
 import Swal from "sweetalert2"
-import Image from 'next/image'
 import Loader from '@/components/Loader/Loader'
-const URL_BASE_ENTREVISTAS = 'https://portfolio-pilaru-back.onrender.com/api/entrevistas'
+const URL_BASE_ENTREVISTAS = 'http://localhost:4000/api/entrevistas'
 
 export default function Subida() {
 
     const ERROR_INESPERADO = 'Error inesperado, contacte con el administrador de la web'
     const [isLoading, setIsLoading] = useState(false)
     const [titulo, setTitulo] = useState('')
-    const [selectedVideo, setSelectedVideo] = useState('')
-    const [selectedImage, setSelectedImage] = useState(null)
-    const [imagePreview, setImagePreview] = useState(null)
-    const [nombreEntrevista, setNombreEntrevista] = useState(null)
+    const [entrevista, setEntrevista] = useState('')
 
+
+    const handleEntrevistaChange = (e) => {
+        setEntrevista(e.target.value)
+        console.log(e.target.value)
+    }
 
     const handleTituloChange = (e) => {
         setTitulo(e.target.value)
-    }
-
-    const handleVideoChange = (e) => {
-        const file = e.target.files[0]
-        if (file) {
-            setNombreEntrevista(file.name)
-            setSelectedVideo(file)
-        }
-    }
-
-    const handleImageChange = (e) => {
-        const file = e.target.files[0]
-        if (file) {
-            setSelectedImage(file)
-
-            // Crear una vista previa de la imagen
-            const reader = new FileReader()
-            reader.onload = (e) => {
-                setImagePreview(e.target.result)
-            }
-            reader.readAsDataURL(file)
-        }
+        console.log(e.target.value)
     }
 
     async function enviarDatos() {
         setIsLoading(true)
         const formData = new FormData
-        formData.append("entrevista", selectedVideo)
-        formData.append("img", selectedImage)
         formData.append("titulo", titulo)
+        formData.append("entrevista", entrevista)
+        console.log(formData)
+
         try {
             await fetch(URL_BASE_ENTREVISTAS, {
                 method: "POST",
                 body: formData
             })
-                .then(res => res.json())
-                .then(data => {
-                    setIsLoading(false)
-                    if (data.code == 409) {
-                        Swal.fire({
-                            icon: "error",
-                            title: data.message,
-                        })
+                .then(res => {
+                    if (res.status == 409) {
+                        res.json()
+                            .then(data => {
+                                setIsLoading(false)
+                                Swal.fire({
+                                    icon: "error",
+                                    title: data.message,
+                                })
+                            })
                     }
                     else {
-                        Swal.fire({
-                            icon: "success",
-                            title: data.message,
-                        }).then(() => {
-                            window.location.reload()
-                        })
+                        res.json()
+                            .then(data => {
+                                setIsLoading(false)
+                                Swal.fire({
+                                    icon: "success",
+                                    title: data.message,
+                                }).then(() => {
+                                    window.location.reload()
+                                })
+                            })
                     }
                 })
         }
@@ -84,6 +71,7 @@ export default function Subida() {
             })
         }
     }
+
 
     if (isLoading) {
         return <Loader />
@@ -110,47 +98,19 @@ export default function Subida() {
                         </div>
                         <div>
                             <div>
-                                <div className='flex space-x-24'>
-                                    <label className="block py-1 cursor-pointer text-blue-500 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded w-1/2 mb-8 text-sm">
-                                        <input
-                                            type="file"
-                                            accept="image/*"
-                                            className="hidden"
-                                            name="img"
-                                            onChange={handleImageChange}
-                                        />
-                                        {'Seleccionar imagen'}
-                                    </label>
-                                </div>
-                            </div>
-                            <div>
-                                <div className='flex space-x-24'>
-                                    <label className="block py-1 cursor-pointer text-blue-500 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded w-1/2 mb-8 text-sm">
-                                        <input
-                                            type="file"
-                                            accept="audio/*"
-                                            className="hidden"
-                                            name="video"
-                                            onChange={handleVideoChange} />
-                                        {`Seleccionar entrevista`}
-                                    </label>
-                                    <BotonVolver url={"/subidas"} />
-                                </div>
-                                {nombreEntrevista && (
-                                    <div className='flex justify-center items-center mb-6'>
-                                        <p className='font-bold'>Estás subiendo la entrevista:</p>
-                                        <p className="block py-1 text-blue-500 ml-6 font-semibold w-1/2 text-sm"> {nombreEntrevista}</p>
-                                    </div>
-                                )}
-                                {imagePreview && (
-                                    <Image
-                                        alt="Vista previa de la imagen a subir"
-                                        className="w-64 h-64 object-cover mx-auto m-4"
-                                        src={imagePreview}
-                                        width={200}
-                                        height={200}
+                                <div className='pb-8'>
+                                    <label htmlFor="text" className="block text-gray-600 font-bold">Link de la entrevista</label>
+                                    <input
+                                        type="text"
+                                        id="entrevista"
+                                        className="mt-1 p-2 w-full border rounded-md"
+                                        placeholder="Inserta el link de la entrevista"
+                                        value={entrevista}
+                                        onChange={handleEntrevistaChange}
+                                        required
                                     />
-                                )}
+                                </div>
+                                <BotonVolver url={"/subidas"} />
                             </div>
                             <button
                                 type='button'
